551ac581320d1619ca2e6dfac4561ab5c089b3f5
aierui <aieruishi@gmail.com>
2020-07-11 21:41:11 +0800

    c

--- lang-operator/src/main/scala/com/aierui/scala/collections/Lists.scala
+++ lang-operator/src/main/scala/com/aierui/scala/collections/Lists.scala
@@ -17,7 +17,7 @@ object Lists {
     val dim: List[List[Int]] = List(
       List(1, 2, 3),
       List(4, 5, 6),
-      List(7, 8, 9),
+      List(7, 8, 9)
     )
 
     println("site Head : " + site.head)
